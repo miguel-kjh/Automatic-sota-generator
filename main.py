8576e9166916611d59221417079996fc79ec5739
miguel-kjh <miguelangel.kjh@gmail.com>
2023-01-17 13:55:01 +0000

    add keyword extraction from nltk

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from arxiv import Search, SortCriterion
 import pandas as pd
 from tqdm import tqdm
+from rake_nltk import Rake
+
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 
 import warnings
 warnings.filterwarnings("ignore")
 
 keywords = "NLU, GPT3"
 year = 2019
+r = Rake()
 
 search = Search(
     query=keywords,
@@ -14,16 +20,21 @@ search = Search(
     sort_by = SortCriterion.SubmittedDate
 )
 
-df = pd.DataFrame(columns=["title", "authors", "summary", "categories", "year"])
+df = pd.DataFrame(columns=["title", "authors", "summary", "categories", "year", "keywords"])
 
 for result in tqdm(search.results(), desc="Papers found"):
     if result.published.year >= year:
+        r.extract_keywords_from_text(result.summary)
+        ranked_phrases = r.get_ranked_phrases_with_scores()
+        ranked_phrases = sorted(ranked_phrases, key=lambda x: x[1], reverse=True)
+        top_phrases = ranked_phrases[:5]
         df = df.append({
             "title": result.title,
             "authors": result.authors,
             "summary": result.summary,
             "categories": result.categories,
-            "year": result.published.year
+            "year": result.published.year,
+            "keywords": top_phrases
         }, ignore_index=True)
 
 #sort by years
